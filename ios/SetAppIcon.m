4774a841693c42c70849db3f877924274d21efdf
Emanuel Quimper <quimperemanuel@gmail.com>
2020-06-04 10:25:43 -0400

    Add better ios 10.3 check

--- ios/SetAppIcon.m
+++ ios/SetAppIcon.m
@@ -1,3 +1,4 @@
+#import "React/RCTLog.h"
 #import "SetAppIcon.h"
 
 @implementation SetAppIcon
@@ -8,35 +9,57 @@ RCT_EXPORT_MODULE()
 
 RCT_REMAP_METHOD(supportsDynamicAppIcon, resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-  bool supported = [[UIApplication sharedApplication] supportsAlternateIcons];
-  resolve(@(supported));
+    if (@available(iOS 10.3, *)) {
+        bool supported = [[UIApplication sharedApplication] supportsAlternateIcons];
+        resolve(@(supported));
+    } else {
+        resolve(@(NO));
+    }
 }
 
 RCT_REMAP_METHOD(changeIcon, iconName:(NSString *)iconName resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
     NSError *error = nil;
 
     // Not supported
-    if ([[UIApplication sharedApplication] supportsAlternateIcons] == NO) {
-        reject(@"Error", @"Alternate icon not supported", error);
+    if (@available(iOS 10.3, *)) {
+        if ([[UIApplication sharedApplication] supportsAlternateIcons] == NO) {
+            reject(@"Error", @"Alternate icon not supported", error);
+            RCTLog(@"Alternate icons are not supported");
+            return;
+        }
+    } else {
+        reject(@"Error", @"Need IOS Version 10.3 at least", error);
         RCTLog(@"Alternate icons are not supported");
         return;
     }
 
-    NSString *currentIcon = [[UIApplication sharedApplication] alternateIconName];
+    if (@available(iOS 10.3, *)) {
+        NSString *currentIcon = [[UIApplication sharedApplication] alternateIconName];
 
-    // If icon is already in use
-    if ([iconName isEqualToString:currentIcon]) {
-        reject(@"Error", @"Icon already in use", error);
-        RCTLog(@"Icon already in use");
+        // If icon is already in use
+        if ([iconName isEqualToString:currentIcon]) {
+            reject(@"Error", @"Icon already in use", error);
+            RCTLog(@"Icon already in use");
+            return;
+        }
+    } else {
+        reject(@"Error", @"Need IOS Version 10.3 at least", error);
+        RCTLog(@"Alternate icons are not supported");
         return;
     }
 
     resolve(@(YES));
 
     // Custom icon
-    [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
-        RCTLog(@"%@", [error description]);
-    }];
+    if (@available(iOS 10.3, *)) {
+        [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+            RCTLog(@"%@", [error description]);
+        }];
+    } else {
+        reject(@"Error", @"Need IOS Version 10.3 at least", error);
+        RCTLog(@"Alternate icons are not supported");
+        return;
+    }
 }
 
 RCT_EXPORT_METHOD(getIconName:(RCTResponseSenderBlock) callback){
@@ -44,11 +67,17 @@ RCT_EXPORT_METHOD(getIconName:(RCTResponseSenderBlock) callback){
     NSDictionary *results;
 
     if( SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.3") ){
-        if( [[UIApplication sharedApplication] supportsAlternateIcons ] ){
-            name = [[UIApplication sharedApplication] alternateIconName];
-            if(name == nil){
-                name = @"default";
+        if (@available(iOS 10.3, *)) {
+            if( [[UIApplication sharedApplication] supportsAlternateIcons ] ){
+                name = [[UIApplication sharedApplication] alternateIconName];
+                if(name == nil){
+                    name = @"default";
+                }
             }
+        } else {
+            reject(@"Error", @"Need IOS Version 10.3 at least", error);
+            RCTLog(@"Alternate icons are not supported");
+            return;
         }
     }
 
